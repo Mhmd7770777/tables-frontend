6ebf18d0a0fd2d5c85f62096db9652bee35b2c24
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-10-01 17:50:30 +0300

    added first prototype of table comp

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import ArtistsTable from "../components/Organisms/ArtistsTable/ArtistsTable";
 import LoginForm from "../components/Organisms/LoginForm/LoginForm";
 
 export default function Home() {
   return (
     <div>
       <LoginForm />
+      <ArtistsTable />
     </div>
   );
 }
