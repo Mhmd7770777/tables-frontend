b7dab3d7605eb41a321bee22b4eaa867e5531f68
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-10-01 18:53:59 +0300

    first prototype of login/register page

--- components/Organisms/LoginForm/style.js
+++ components/Organisms/LoginForm/style.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  margin-top: 24px;
-  max-width: 60%;
-
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   .ant-btn {
     margin-right: 12px;
+    background-color: black;
+  }
+
+  .ant-form {
+    border: 0.5px solid black;
+    border-radius: 35px;
+    padding: 24px;
+    padding-top: 48px;
+    filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04))
+      drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));
   }
 `;
---
+++ pages/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Login from "../components/Template/Login";
+
+const LoginPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
